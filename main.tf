86be3eee6995b6a4438b8dc4e72bfd3c18a56877
Denis Sulgin <sulgin.ds@med54.ru>
2019-10-22 09:14:09 +0700

    format conf files via 'terraform fmt'

--- main.tf
+++ main.tf
@@ -4,65 +4,69 @@ terraform {
 
 provider "google" {
   version = "2.0.0"
-#  project = "ruby-app-denriful"
+
+  #  project = "ruby-app-denriful"
   project = "${var.project}"
-#  region = "us-central1"
+
+  #  region = "us-central1"
   region = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-  name		= "reddit-app"
-  machine_type  = "g1-small"
-#  zone		= "us-central1-a"
-  zone		= "${var.zone}"
+  name         = "reddit-app"
+  machine_type = "g1-small"
+
+  #  zone		= "us-central1-a"
+  zone = "${var.zone}"
   tags = ["reddit-app"]
 
   boot_disk {
     initialize_params {
-#      image = "reddit-base"
+      #      image = "reddit-base"
       image = "${var.disk_image}"
     }
   }
 
   network_interface {
     network = "default"
-# use ethemeral ip for access from outside
+
+    # use ethemeral ip for access from outside
     access_config {}
   }
 
   metadata {
-# path to public key
-#  ssh-keys = "appuser:${file("/home/sulgin/.ssh/appuser.pub")}"
-  ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+    # path to public key
+    #  ssh-keys = "appuser:${file("/home/sulgin/.ssh/appuser.pub")}"
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
   }
 
-# connection settings for provisioners
+  # connection settings for provisioners
   connection {
-  type = "ssh"
-  user = "appuser"
-  agent = false
-  private_key = "${file(var.provision_private_key)}"
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.provision_private_key)}"
   }
 
   provisioner "file" {
-  source = "files/puma.service"
-  destination = "/tmp/puma.service"
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
   }
 
   provisioner "remote-exec" {
-  script = "files/deploy.sh"
+    script = "files/deploy.sh"
   }
-
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+  name    = "allow-puma-default"
   network = "default"
+
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
-}
 
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
--- variables.tf
+++ variables.tf
@@ -1,19 +1,24 @@
 variable "gce_ssh_user" {}
 variable "gce_ssh_pub_key_file" {}
+
 variable project {
   description = "Project ID"
 }
+
 variable region {
   description = "Region"
-  default = "us-central1"
+  default     = "us-central1"
 }
+
 variable zone {
   description = "Zone"
-  default = "us-central1-a"
+  default     = "us-central1-a"
 }
+
 variable disk_image {
   description = "Disk image"
 }
+
 variable provision_private_key {
   description = "Private key for provisioners"
 }
