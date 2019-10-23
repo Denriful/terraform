621f511c24bff1e44d2a990f08023aaf82483503
Denis Sulgin <sulgin.ds@med54.ru>
2019-10-23 16:29:30 +0700

    redesign project with modules

--- main.tf
+++ main.tf
@@ -5,10 +5,8 @@ terraform {
 provider "google" {
   version = "2.0.0"
 
-  #  project = "ruby-app-denriful"
   project = "${var.project}"
 
-  #  region = "us-central1"
   region = "${var.region}"
 
 }
@@ -22,89 +20,15 @@ resource "google_compute_project_metadata" "default" {
 
 }
 
-
-resource "google_compute_instance" "app" {
-#  name         = "ruby-app"
-  name         = "ruby-app-${count.index + 1}"
-  count	       = "${var.count}"	
-#  machine_type = "g1-small"
-  machine_type = "f1-micro"
-
-  #  zone		= "us-central1-a"
-  zone = "${var.zone}"
-  tags = ["ruby-app"]
-
-  boot_disk {
-    initialize_params {
-      #      image = "ruby-base"
-      image = "${var.disk_image}"
-    }
-  }
-
-  scheduling {
-    preemptible = "True"
-    automatic_restart = "False"
-  }
-
-  network_interface {
-    network = "default"
-
-    # use ethemeral ip for access from outside
-    access_config {
-      nat_ip = "${google_compute_address.app_ip.address}"
-    }
-  }
-
-  metadata {
-    # path to public key
-    #  ssh-keys = "appuser:${file("/home/sulgin/.ssh/appuser.pub")}"
-    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
-  }
-
-  # connection settings for provisioners
-  connection {
-    type        = "ssh"
-    user        = "appuser"
-    agent       = false
-    private_key = "${file(var.provision_private_key)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+module "app" {
+  source	= "./modules/app"
+  count		= "2"
 }
 
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ruby-app"]
+module "vpc" {
+  source	= "./modules/vpc"
 }
 
-resource "google_compute_firewall" "firewall_ssh" {
-  name    = "default-allow-ssh"
-  network = "default"
-  description = "Allow SSH from anywhere"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
 
-  source_ranges = ["0.0.0.0/0"]
-}
 
-resource "google_compute_address" "app_ip" {
-  name = "ruby-app-ip"
-}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "gce_ssh_user" {}
-variable "gce_ssh_pub_key_file" {}
+#variable "gce_ssh_user" {}
+#variable "gce_ssh_pub_key_file" {}
 
 variable project {
   description = "Project ID"
@@ -10,19 +10,19 @@ variable region {
   default     = "us-central1"
 }
 
-variable zone {
-  description = "Zone"
-  default     = "us-central1-a"
-}
+#variable zone {
+#  description = "Zone"
+#  default     = "us-central1-a"
+#}
 
-variable disk_image {
-  description = "Disk image"
-}
+#variable disk_image {
+#  description = "Disk image"
+#}
 
-variable provision_private_key {
-  description = "Private key for provisioners"
-}
+#variable provision_private_key {
+#  description = "Private key for provisioners"
+#}
 
-variable count {
-  description = "Number of instances"
-}
+#variable count {
+#  description = "Number of instances"
+#}
