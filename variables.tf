e1c3f639d8cfeca5f733ac7bb6f46987d59b2bae
Denis Sulgin <sulgin.ds@med54.ru>
2019-10-24 14:25:56 +0700

    edit variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,3 @@
-#variable "gce_ssh_user" {}
-#variable "gce_ssh_pub_key_file" {}
-
 variable project {
   description = "Project ID"
 }
@@ -10,19 +7,3 @@ variable region {
   default     = "us-central1"
 }
 
-#variable zone {
-#  description = "Zone"
-#  default     = "us-central1-a"
-#}
-
-#variable disk_image {
-#  description = "Disk image"
-#}
-
-#variable provision_private_key {
-#  description = "Private key for provisioners"
-#}
-
-#variable count {
-#  description = "Number of instances"
-#}
