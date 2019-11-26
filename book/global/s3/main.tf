994ada965ab38c0693a3ba179e9bd480f4b65a88
Denis Sulgin <sulgin.ds@med54.ru>
2019-11-26 13:29:58 +0700

    add comment

--- book/global/s3/main.tf
+++ book/global/s3/main.tf
@@ -48,6 +48,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
    }
 }
 
+# to connect to instance you also need to open port 22
 resource "aws_key_pair" "appuser" {
   key_name   = "appuser"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDX8ik3cnM4WOvAW27LqbYisCtRWAkYKxW0JFMwcNJJHJiTUHyB93oNkwjM+UBvMgMO2W43O7w5AO8z/19N2GfTINFwMgDsj45OtXj97BHZmKB0XRpkdV7r8B7V6GU5L6yJsIdkglfPiPxda8ivVqBobKW5jIZqPfvQCT2mRSuoCZLpAeC8XcK7Rj8/WW+OM74PLkY6uK0kIhdCgy6TeXX/3FL+rg+bjIwas39fa5yo+3BTfvBMzO/Z5t8uyYJaRZzuR6vkxolfWGZNgiyYsyTQj66/OCu8nacxQdQd5du4uMlw4zqzMChAwR2jl/w7uzigmRaEBWBUZ+sSg1xsgvpR appuser"
